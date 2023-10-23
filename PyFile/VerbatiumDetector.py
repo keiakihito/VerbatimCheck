729af05606150f3918b7a3741710a5d96166c69b
keiakihito <58803142+keiakihito@users.noreply.github.com>
2023-10-23 07:44:08 -0700

    Update VerbatiumDetector.py

--- PyFile/VerbatiumDetector.py
+++ PyFile/VerbatiumDetector.py
@@ -1,31 +1,25 @@
 '''
-/**
- * Author: Keita Katsumi
- * Last updated Oct 22. 2023
- * The program  is for super raters' verbatim detection check process.
- * It aims to reduce verbatim manual checking time and effort.
- * But it can check a single row every time.
- *
- * The VerbatimCheck program does the three things
- *
- * 1. Read three text files: rewrite.txt, model.txt, and reference txt
- * All the text information comes form the raters' google sheet manually.
- * Extract the text data as a String, then store each sentence in the ArrayList<String>
- *
- * 2. Compare raters' rewrite and model response.
- * The program checks the number of consecutive letters in the rewrite in the model response.
- * If it is detected, the program shows
- *   i. Consecutive letters,
- *   ii. Rewrite sentence which has the consecutive letters,
- *   iii. Model response sentence that has consecutive letters.
- *
- *   3. Compare the raters' rewrite and references.
- *   It takes the same process as section 2.
- *
- *   If you would like to discuss the future application or any issues,
- *   Please contact my Google or GL email.
- *
- */
+The program is for SR's verbatim detection check process. It aims to reduce verbatim manual checking time and effort. But it can check a single row every time.
+
+    The VerbatimCheck program does three things.
+
+    Read three text files: rewrite.txt, model.txt, and reference txt
+    All the text information comes from the spreadsheet manually.
+    Extract the text data as a String, then store each sentence in the list object.
+
+    Compare SR's rewrite and model response.
+    The program checks the number of consecutive letters in the rewrite in the model response.
+    If it is detected, the program shows
+    i. Consecutive letters,
+    ii. Rewrite the sentence which has the consecutive letters,
+    iii. Model response sentence that has consecutive letters.
+
+    Compare the raters' rewrite and references.
+    It takes the same process as section 2.
+
+
+If you would like to discuss the future application or any issues,
+please contact my email.
 '''
 
 import re
